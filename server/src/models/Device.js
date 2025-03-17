04dc0668803ed0bbf814d895078dc54deb2a6a55
Ivaylo Andonov <nikb250nik@gmail.com>
2025-03-17 18:14:21 +0200

    Update Tech Validation

--- server/src/models/Device.js
+++ server/src/models/Device.js
@@ -5,51 +5,63 @@ import { model, Schema, Types } from "mongoose";
 const deviceSchema = new Schema({
     brand:{
         type:String,
-        required:true
+        required:true,
+        minLength:2
     },
     model:{
         type:String,
         required:true
+        ,minLength:2
     },
     hardDisk:{
-        type:String,
-        required:true
+        type:Numbear,
+        required:true,
+        min:128
     },
     screenSize:{
-        type:String,
-        required:true
+        type:Number,
+        required:true,
+        min:10
     },
     ram:{
-        type:String,
-        required:true
+        type:Number,
+        required:true,
+        min:4
     },
     os:{
         type:String,
-        required:true
+        required:true,
+        minLength:2
     },
     cpu:{
         type:String,
-        required:true
+        required:true,
+        minLength:2
     },
     gpu:{
         type:String,
-        required:true
+        required:true,
+        minLength:2
     },
     price:{
         type:Number,
-        required:true
+        required:true,
+        min:100
     },
     colour:{
         type:String,
-        required:true
+        required:true,
+        minLength:2
     },
     weight:{
-        type:String,
+        type:Number,
         required:true
+        ,min:300
     },
     imageUrl:{
         type:String,
-        required:true
+        required:true,
+        match:/^https?:\/\//
     },
     owner:{
         type: Types.ObjectId,
