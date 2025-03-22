867be91ab09706662461bd8c1bceeb3fc3457a7b
Ivaylo Andonov <nikb250nik@gmail.com>
2025-03-22 17:41:01 +0200

    add username to authcontext

--- client/src/contexts/authContext.jsx
+++ client/src/contexts/authContext.jsx
@@ -10,39 +10,42 @@ export const AuthContext = createContext({
 
 export function AuthContextProvider(props) {
     const [authState, setAuthState] = useState(() => {
-       
         const token = localStorage.getItem("token");
         const email = localStorage.getItem("email");
         const _id = localStorage.getItem("_id");
+        const username = localStorage.getItem("username");
 
-        return token && email && _id ? { token, email, _id } : {};
+        return token && email && _id && username ? { token, email, _id, username } : {};
     });
 
     const changeAuthState = (state) => {
-            localStorage.setItem("token", state.token);
-            localStorage.setItem("email", state.email);
-            localStorage.setItem("_id", state._id);
-            localStorage.setItem("username", state.username);
+        localStorage.setItem("token", state.token);
+        localStorage.setItem("email", state.email);
+        localStorage.setItem("_id", state._id);
+        localStorage.setItem("username", state.username);
         setAuthState(state);
     };
+
     const logout = () => {
         localStorage.clear();
         setAuthState({});
     };
+
     const contextData = {
         email: authState.email,
         token: authState.token,
         userId: authState._id,
-        username:authState.username,
+        username: authState.username,
         isAuthenticated: !!authState._id,
         changeAuthState,
         logout
     };
+
     return (
         <AuthContext.Provider value={contextData}>
             {props.children}
         </AuthContext.Provider>
-    )
+    );
 }
 
 export function useAuthContext() {
